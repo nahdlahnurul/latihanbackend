711ed00febb2d45b7967c8d78369a80542a1da41
nahdlahnurul <46091484+nahdlahnurul@users.noreply.github.com>
2019-09-21 23:47:25 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$bangunruangsegitiga = "Segitiga";
+echo $bangunruangsegitiga;
+echo "  ";
+
+$alas=20;
+$tinggi=10;
+$luas_segitiga = 1/2 * $alas * $tinggi; // Menghitung Luas segitiga
+echo "Luas : $luas_segitiga <br>";
+
+$bangunruangjajargenjang = "Jajaran Genjang";
+echo $bangunruangjajargenjang;
+echo "  ";
+
+$alas=40;
+$tinggi=10;
+$luas_jajargenjang = $alas * $tinggi; // Menghitung Luas Jajaran Genjang
+echo "Luas : $luas_jajargenjang <br>";
+
+$bangunruanglingkaran = "Lingkaran";
+echo $bangunruanglingkaran;
+echo "  ";
+
+$phi=22/7;
+$jari2=7;
+$luas_lingkaran = $phi * $jari2 * $jari2; // Menghitung Luas lingkaran
+echo "Luas : $luas_lingkaran <br>";
